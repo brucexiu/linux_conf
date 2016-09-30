c6dd89550b6080bab429946fb9a7887b2f539658
xiuzhixin <brucexiu@gmail.com>
2016-09-30 16:08:26 +0800

    add ssh.sh

---
+++ ssh.sh
@@ -0,0 +1,3 @@
+mkdir -p -m 700 ~/.ssh
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQClckZvH/TjmjiAjiyWO5GevyoeFofy7Oo6UQxqoSeuRdd66QlAGiT8GxQTbZTQ9CfoVJ+VZbeEnPAui0sgc5HnX9tnINRZxekPOIMGznPtE6p04FA1eFjNwbt1fx9KY+SQGJ4KyHkuekg370HgGFTbYDMkyF2SI2QViL8TVH5P22hsfc/0aaYEHZGzSo5dE248N4rQNptJ2IYOj72TUaRhYfBjMSEs/sFKtzGm2PgNTqi2FrUNNbkDngeM9c8C04Y/NuCT1DRLmr6p72omcsal60+WsMBA7kz8pbd8qkcnmy1kbMi+KQ2WRxs+U8hxVRXhCSL/vzyk2Tpsu6VXkATn brucexiu@gmail.com" >> ~/.ssh/authorized_keys
+chmod 400 ~/.ssh/authorized_keys
